a265e4dbd3927120175bb47bc1b7632e75b8be3c
Shubham Mishra <ramsmishra111@gmail.com>
2021-01-25 16:53:18 +0530

    UI improvement

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,14 +3,22 @@
 
 module ApplicationHelper
   def home_btn
-    link_to('<i class="fab fa-twitter fa-2x"></i>'.html_safe, root_path, class: 'text-white pl-4 pt-1')
+    link_to('<i class="fab fa-twitter fa-2x"></i>'.html_safe, root_path, class: 'text-white px-3 pt-1')
+  end
+
+  def users_btn
+    link_to('<i class="fas fa-users fa-2x"></i>'.html_safe, users_path, class: 'text-white px-3 pt-1')
+  end
+
+  def profile_btn
+    link_to('<i class="fas fa-user-circle fa-2x"></i>'.html_safe, user_path(current_user), class: 'text-white px-3 pt-1') if current_user
   end
 
   def nav_btns
     if current_user
-      link_to('Log out', logout_path, class: 'text-light text-decoration-none btn btn-secondary')
+      link_to('Log out', logout_path, class: 'text-dark text-decoration-none btn btn-light')
     else
-      link_to('Sign in', login_path, class: 'text-dark text-decoration-none btn btn-light') + link_to('Sign up', new_user_path, class: 'text-dark text-decoration-none btn btn-light')
+      link_to('Sign in', login_path, class: 'text-dark text-decoration-none btn btn-light mr-3') + link_to('Sign up', new_user_path, class: 'text-dark text-decoration-none btn btn-light')
     end
   end
 
